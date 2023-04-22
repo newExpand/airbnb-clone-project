cfe6f85c373ba409895e7efade27b739cd5332c7
newExpand <magin0auto@gmail.com>
2023-04-22 19:46:00 +0900

    feat: 로그인 관련 모달창 컴포넌트 생성 및 스타일링 완료

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -22,6 +22,8 @@ const Button: React.FC<ButtonProps> = ({
 }) => {
     return (
         <button
+            onClick={onClick}
+            disabled={disabled}
             className={`relative disabled:opacity-70 disabled:cursor-not-allowed rounded-lg hover:opacity-80 transition w-full 
                 ${outline ? "bg-white" : "bg-rose-500"}
                 ${outline ? "border-black" : "bg-rose-500"}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
+"use client";
 import { Noto_Sans_KR } from "next/font/google";
 
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
 
 export const metadata = {
     title: "Airbnb",
@@ -16,10 +18,21 @@ const font = Noto_Sans_KR({
 });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+    const handleClose = () => {};
+
+    const handleSubmit = () => {};
+
     return (
         <html lang="ko">
             <body className={font.className}>
                 <ClientOnly>
+                    <Modal
+                        title="여러분 안뇽?"
+                        isOpen
+                        onClose={handleClose}
+                        onSubmit={handleSubmit}
+                        actionLabel="제출"
+                    />
                     <Navbar />
                 </ClientOnly>
                 {children}
