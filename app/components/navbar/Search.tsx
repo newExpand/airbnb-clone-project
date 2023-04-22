589d2a1fa3550b904f81ef0e0551445c92220248
newExpand <magin0auto@gmail.com>
2023-04-22 12:09:20 +0900

    feat: search 컴포넌트 추가 및 스타일링 완료

---
+++ app/components/navbar/Search.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import React from "react";
+import { BiSearch } from "react-icons/bi";
+
+const Search = () => {
+    return (
+        <div className="border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer">
+            <div className="flex flex-row items-center justify-between">
+                <div className="px-6 text-sm font-semibold">어디든지</div>
+                <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">
+                    언제든 일주일
+                </div>
+                <div className="flex flex-row items-center gap-3 pl-6 pr-2 text-sm text-gray-600">
+                    <div className="hidden sm:block">게스트 추가</div>
+                    <div className="p-2 text-white rounded-full bg-rose-500">
+                        <BiSearch size={18} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Search;
