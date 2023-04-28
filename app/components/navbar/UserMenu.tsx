5847aa284042097d23ddc9a1ed821fdee667f7a0
newExpand <magin0auto@gmail.com>
2023-04-28 18:33:38 +0900

    feat: 로그인 시 보이는 메뉴아이템과 로그아웃 했을 때 보이는 메뉴아이템 추가 및 로그아웃 기능 구현

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -6,8 +6,14 @@ import Avatar from "../Avatar";
 import MenuItem from "./MenuItem";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import useLoginModal from "@/app/hooks/useLoginModal";
+import { User } from "@prisma/client";
+import { signOut } from "next-auth/react";
 
-const UserMenu = () => {
+interface UserMenuProps {
+    currentUser?: User | null;
+}
+
+const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
     const registerModal = useRegisterModal();
     const loginModel = useLoginModal();
     const [isOpen, setIsOpen] = useState(false);
@@ -38,10 +44,29 @@ const UserMenu = () => {
             {isOpen && (
                 <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
                     <div className="flex flex-col cursor-pointer">
-                        <>
-                            <MenuItem onClick={registerModal.onOpen} label="회원가입" />
-                            <MenuItem onClick={loginModel.onOpen} label="로그인" />
-                        </>
+                        {currentUser ? (
+                            <>
+                                <MenuItem onClick={() => {}} label="나의 여행" />
+                                <MenuItem onClick={() => {}} label="나의 위시리스트" />
+                                <MenuItem onClick={() => {}} label="나의 예약" />
+                                <hr />
+                                <MenuItem onClick={() => {}} label="계정" />
+                                <MenuItem
+                                    onClick={() => {}}
+                                    label="당신의 공간을 에어비앤비하세요"
+                                />
+                                <hr />
+                                <MenuItem onClick={() => signOut()} label="로그아웃" />
+                            </>
+                        ) : (
+                            <>
+                                <MenuItem
+                                    onClick={registerModal.onOpen}
+                                    label="회원가입"
+                                />
+                                <MenuItem onClick={loginModel.onOpen} label="로그인" />
+                            </>
+                        )}
                     </div>
                 </div>
             )}
