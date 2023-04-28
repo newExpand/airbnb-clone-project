1c0f6a56b512951e151a33569a3587dcdd7c425a
newExpand <magin0auto@gmail.com>
2023-04-28 14:03:30 +0900

    rename: 파일명 오타 수정

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -9,7 +9,7 @@ import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
 
-import useRegisterModal from "@/app/hooks/useRegisterModa";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
 import Modal from "./Modal";
 import Heading from "../Heading";
 import Input from "../inputs/Input";
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -4,7 +4,7 @@ import React, { useCallback, useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
 import MenuItem from "./MenuItem";
-import useRegisterModal from "@/app/hooks/useRegisterModa";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
 
 const UserMenu = () => {
     const registerModal = useRegisterModal();
