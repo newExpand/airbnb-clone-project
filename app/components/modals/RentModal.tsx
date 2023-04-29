3a01bd6c3159ed8cb9a2c6ed0e0bb31198e9f18a
newExpand <magin0auto@gmail.com>
2023-04-30 01:48:24 +0900

    feat: RentModal 안의 카테고리인풋 UI 구현 - 다음 이전 로직 코드 추가 - selected 조건 로직 코드 추가

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -1,19 +1,122 @@
 "use client";
 
-import React from "react";
+import React, { useMemo, useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
 import Modal from "./Modal";
 import useRentModal from "@/app/hooks/useRentModal";
+import Heading from "../Heading";
+import { categories } from "../navbar/Categories";
+import CategoryInput from "../inputs/CategoryInput";
+
+enum STEPS {
+    CATEGORY = 0,
+    LOCATION = 1,
+    INFO = 2,
+    IMAGES = 3,
+    DESCRIPTION = 4,
+    PRICE = 5,
+}
 
 const RentModal = () => {
     const rentModal = useRentModal();
+
+    const [step, setStep] = useState(STEPS.CATEGORY);
+
+    const {
+        register,
+        handleSubmit,
+        setValue,
+        watch,
+        formState: { errors },
+        reset,
+    } = useForm<FieldValues>({
+        defaultValues: {
+            category: "",
+            location: null,
+            guestCount: 1,
+            roomCount: 1,
+            bathroomCount: 1,
+            imageSrc: "",
+            price: 1,
+            title: "",
+            description: "",
+        },
+    });
+
+    const category = watch("category");
+
+    const setCustomValue = (id: string, value: any) => {
+        setValue(id, value, {
+            shouldValidate: true,
+            shouldDirty: true,
+            shouldTouch: true,
+        });
+    };
+
+    const onBack = () => {
+        setStep((value) => value - 1);
+    };
+
+    const onNext = () => {
+        setStep((value) => value + 1);
+    };
+
+    const actionLabel = useMemo(() => {
+        if (step === STEPS.PRICE) {
+            return "등록 완료";
+        }
+
+        return "다음";
+    }, [step]);
+
+    const secondaryActionLabel = useMemo(() => {
+        if (step === STEPS.CATEGORY) {
+            return undefined;
+        }
+
+        return "이전";
+    }, [step]);
+
+    let bodyContent = (
+        <div className="flex flex-col gap-8">
+            <Heading
+                title="최고의 표현으로 당신의 장소를 설명해주세요"
+                subtitle="카테고리를 선택해주세요"
+            />
+            <div
+                className="
+                    grid
+                    grid-cols-1
+                    md:grid-cols-2
+                    gap-3
+                    max-h-[50vh]
+                    overflow-y-auto
+                "
+            >
+                {categories.map((item) => (
+                    <div key={item.label} className="col-span-1">
+                        <CategoryInput
+                            onClick={(category) => setCustomValue("category", category)}
+                            selected={category === item.label}
+                            label={item.label}
+                            icon={item.icon}
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 
     return (
         <Modal
             isOpen={rentModal.isOpen}
             onClose={rentModal.onClose}
             onSubmit={rentModal.onClose}
-            actionLabel="제출하기"
+            actionLabel={actionLabel}
+            secondaryActionLabel={secondaryActionLabel}
+            secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
             title="당신의 공간을 에어비앤비하세요"
+            body={bodyContent}
         />
     );
 };
