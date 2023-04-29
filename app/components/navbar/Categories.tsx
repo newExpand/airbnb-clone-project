8cf1fa65e025961a8b055f2140eab4db176c7751
newExpand <magin0auto@gmail.com>
2023-04-29 20:12:55 +0900

    feat: 카테고리 아이템 목록 추가

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -2,8 +2,21 @@
 
 import React from "react";
 import Container from "../Container";
-import { TbBeach } from "react-icons/tb";
-import { GiWindmill } from "react-icons/gi";
+import { TbBeach, TbPool } from "react-icons/tb";
+import {
+    GiWindmill,
+    GiCastle,
+    GiForestCamp,
+    GiIsland,
+    GiBoatFishing,
+    GiCaveEntrance,
+    GiCactus,
+    GiBarn,
+} from "react-icons/gi";
+import { BiLandscape } from "react-icons/bi";
+import { IoDiamond } from "react-icons/io5";
+import { BsSnow } from "react-icons/bs";
+import { FaSkiing } from "react-icons/fa";
 import { MdOutlineVilla } from "react-icons/md";
 import CategoryBox from "../CategoryBox";
 import { usePathname, useSearchParams } from "next/navigation";
@@ -22,6 +35,66 @@ export const categories = [
     {
         label: "디자인",
         icon: MdOutlineVilla,
+        description: "",
+    },
+    {
+        label: "한적한 시골",
+        icon: BiLandscape,
+        description: "",
+    },
+    {
+        label: "멋진 수영장",
+        icon: TbPool,
+        description: "",
+    },
+    {
+        label: "섬",
+        icon: GiIsland,
+        description: "",
+    },
+    {
+        label: "호숫가",
+        icon: GiBoatFishing,
+        description: "",
+    },
+    {
+        label: "스키",
+        icon: FaSkiing,
+        description: "",
+    },
+    {
+        label: "캐슬",
+        icon: GiCastle,
+        description: "",
+    },
+    {
+        label: "캠핑장",
+        icon: GiForestCamp,
+        description: "",
+    },
+    {
+        label: "북극",
+        icon: BsSnow,
+        description: "",
+    },
+    {
+        label: "동굴",
+        icon: GiCaveEntrance,
+        description: "",
+    },
+    {
+        label: "사막",
+        icon: GiCactus,
+        description: "",
+    },
+    {
+        label: "헛간",
+        icon: GiBarn,
+        description: "",
+    },
+    {
+        label: "Luxe",
+        icon: IoDiamond,
         description: "",
     },
 ];
