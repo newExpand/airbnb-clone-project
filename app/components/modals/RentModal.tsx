20cccfa25b03a2c297d2fbb6ae5ba090b6d3694c
newExpand <magin0auto@gmail.com>
2023-04-30 17:27:25 +0900

    feat: 스텝절차를 전부 진행 후 최종 등록 로직 구현

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -1,15 +1,21 @@
 "use client";
 
 import React, { useMemo, useState } from "react";
-import { FieldValues, useForm } from "react-hook-form";
-import Modal from "./Modal";
+
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import dynamic from "next/dynamic";
+import { useRouter } from "next/navigation";
+
 import useRentModal from "@/app/hooks/useRentModal";
+
+import Modal from "./Modal";
+import Counter from "../inputs/Counter";
+import CategoryInput from "../inputs/CategoryInput";
 import Heading from "../Heading";
 import { categories } from "../navbar/Categories";
-import CategoryInput from "../inputs/CategoryInput";
 import CountrySelect from "../inputs/CountrySelect";
-import dynamic from "next/dynamic";
-import Counter from "../inputs/Counter";
 import ImageUpload from "../inputs/ImageUpload";
 import Input from "../inputs/Input";
 
@@ -23,6 +29,7 @@ enum STEPS {
 }
 
 const RentModal = () => {
+    const router = useRouter();
     const rentModal = useRentModal();
 
     const [isLoading, setIsLoading] = useState(false);
@@ -78,6 +85,30 @@ const RentModal = () => {
 
     const onNext = () => {
         setStep((value) => value + 1);
+    };
+
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+        if (step !== STEPS.PRICE) {
+            return onNext();
+        }
+
+        setIsLoading(true);
+
+        axios
+            .post("/api/listings", data)
+            .then(() => {
+                toast.success("Listing created!");
+                router.refresh();
+                reset();
+                setStep(STEPS.CATEGORY);
+                rentModal.onClose();
+            })
+            .catch(() => {
+                toast.error("Something went wrong.");
+            })
+            .finally(() => {
+                setIsLoading(false);
+            });
     };
 
     const actionLabel = useMemo(() => {
