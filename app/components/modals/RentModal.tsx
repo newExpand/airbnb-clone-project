6acb915b338b99010c7b3ba184bcf33892666262
newExpand <magin0auto@gmail.com>
2023-04-30 10:49:30 +0900

    feat: 3Step 숙소 기본정보 UI 구현 - 게스트, 침실, 욕실 카운트 로직 완료

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -9,6 +9,7 @@ import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
 import CountrySelect from "../inputs/CountrySelect";
 import dynamic from "next/dynamic";
+import Counter from "../inputs/Counter";
 
 enum STEPS {
     CATEGORY = 0,
@@ -47,6 +48,9 @@ const RentModal = () => {
 
     const category = watch("category");
     const location = watch("location");
+    const guestCount = watch("guestCount");
+    const roomCount = watch("roomCount");
+    const bathroomCount = watch("bathroomCount");
 
     const Map = useMemo(
         () =>
@@ -130,6 +134,37 @@ const RentModal = () => {
                     onChange={(value) => setCustomValue("location", value)}
                 />
                 <Map center={location?.latlng} />
+            </div>
+        );
+    }
+
+    if (step === STEPS.INFO) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading
+                    title="숙소 기본 정보를 알려주세요"
+                    subtitle="침대 유형과 같은 세부 사항은 나중에 추가하실 수 있습니다."
+                />
+                <Counter
+                    title="게스트"
+                    subtitle="게스트 인원을 설정해주세요"
+                    value={guestCount}
+                    onChange={(value) => setCustomValue("guestCount", value)}
+                />
+                <hr />
+                <Counter
+                    title="침실"
+                    subtitle="숙소의 침실 갯수를 설정해주세요"
+                    value={roomCount}
+                    onChange={(value) => setCustomValue("roomCount", value)}
+                />
+                <hr />
+                <Counter
+                    title="욕실"
+                    subtitle="숙소의 욕실 갯수를 설정해주세요"
+                    value={bathroomCount}
+                    onChange={(value) => setCustomValue("bathroomCount", value)}
+                />
             </div>
         );
     }
