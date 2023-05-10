a3abfc99c8db6bc315172737a62f145fdfdf5b2a
newExpand <magin0auto@gmail.com>
2023-05-10 14:41:15 +0900

    feat: 카테고리 description 추가

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -25,77 +25,77 @@ export const categories = [
     {
         label: "해변 근처",
         icon: TbBeach,
-        description: "",
+        description: "이 숙소는 해변과 가깝습니다",
     },
     {
         label: "풍차",
         icon: GiWindmill,
-        description: "",
+        description: "이 숙소 근처엔 풍차가 있습니다",
     },
     {
         label: "디자인",
         icon: MdOutlineVilla,
-        description: "",
+        description: "이 숙소는 아름다움을 느낄 수 있습니다",
     },
     {
         label: "한적한 시골",
         icon: BiLandscape,
-        description: "",
+        description: "이 숙소는 시골의 여유로움을 느낄 수 있습니다",
     },
     {
         label: "멋진 수영장",
         icon: TbPool,
-        description: "",
+        description: "이 숙소는 멋진 수영장을 보유하고 있습니다",
     },
     {
         label: "섬",
         icon: GiIsland,
-        description: "",
+        description: "이 숙소는 섬에 위치해 있습니다",
     },
     {
         label: "호숫가",
         icon: GiBoatFishing,
-        description: "",
+        description: "이 숙소는 호숫가 근처에 있습니다",
     },
     {
         label: "스키",
         icon: FaSkiing,
-        description: "",
+        description: "이 숙소 근처에 스키장이 있습니다",
     },
     {
         label: "캐슬",
         icon: GiCastle,
-        description: "",
+        description: "이 숙소는 성처럼 생겼습니다",
     },
     {
         label: "캠핑장",
         icon: GiForestCamp,
-        description: "",
+        description: "이 숙소는 캠핑을 즐길 수 있습니다",
     },
     {
         label: "북극",
         icon: BsSnow,
-        description: "",
+        description: "이 숙소는 북극에 위치해 있습니다",
     },
     {
         label: "동굴",
         icon: GiCaveEntrance,
-        description: "",
+        description: "이 숙소는 동굴을 이용해 만들어졌습니다",
     },
     {
         label: "사막",
         icon: GiCactus,
-        description: "",
+        description: "이 숙소는 사막에 위치해 있습니다",
     },
     {
         label: "헛간",
         icon: GiBarn,
-        description: "",
+        description: "이 숙소는 헛간의 인테리어를 사용해 만들어졌습니다",
     },
     {
         label: "Luxe",
         icon: IoDiamond,
-        description: "",
+        description: "이 숙소는 호캉스를 즐길 수 있습니다",
     },
 ];
 
