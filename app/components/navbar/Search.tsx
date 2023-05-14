fed7f399a05944512659eec4c1baae1e7ed97e83
newExpand <magin0auto@gmail.com>
2023-05-14 19:05:29 +0900

    feat: 필터시 결과값 UI*반영

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,11 +1,54 @@
 "use client";
 
-import useSearchModal from "@/app/hooks/useSearchModal";
-import React from "react";
+import React, { useMemo } from "react";
+import { differenceInDays } from "date-fns";
 import { BiSearch } from "react-icons/bi";
+import { useSearchParams } from "next/navigation";
+
+import useCountries from "@/app/hooks/useCountries";
+import useSearchModal from "@/app/hooks/useSearchModal";
 
 const Search = () => {
     const searchModal = useSearchModal();
+    const params = useSearchParams();
+    const { getByValue } = useCountries();
+
+    const locationValue = params?.get("locationValue");
+    const startDate = params?.get("startDate");
+    const endDate = params?.get("endDate");
+    const guestCount = params?.get("guestCount");
+
+    const locationLabel = useMemo(() => {
+        if (locationValue) {
+            const location = getByValue(locationValue);
+            return `${location?.label} (${location?.translations})`;
+        }
+
+        return "어디든지";
+    }, [getByValue, locationValue]);
+
+    const durationLabel = useMemo(() => {
+        if (startDate && endDate) {
+            const start = new Date(startDate as string);
+            const end = new Date(endDate as string);
+            let diff = differenceInDays(end, start);
+
+            if (diff === 0) {
+                diff = 1;
+            }
+            return `${diff} 일`;
+        }
+
+        return "언제든 일주일";
+    }, [startDate, endDate]);
+
+    const guestLabel = useMemo(() => {
+        if (guestCount) {
+            return `게스트 ${guestCount}명`;
+        }
+
+        return "게스트 추가";
+    }, [guestCount]);
 
     return (
         <div
@@ -13,12 +56,12 @@ const Search = () => {
             className="border-[1px] w-full md:w-auto md:ml-2 py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer"
         >
             <div className="flex flex-row items-center justify-between break-keep">
-                <div className="px-6 text-sm font-semibold">어디든지</div>
+                <div className="px-6 text-sm font-semibold">{locationLabel}</div>
                 <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">
-                    언제든 일주일
+                    {durationLabel}
                 </div>
                 <div className="flex flex-row items-center gap-3 pl-6 pr-2 text-sm text-gray-600">
-                    <div className="hidden sm:block">게스트 추가</div>
+                    <div className="hidden sm:block">{guestLabel}</div>
                     <div className="p-2 text-white rounded-full bg-rose-500">
                         <BiSearch size={18} />
                     </div>
