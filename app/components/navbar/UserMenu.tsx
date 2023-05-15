b511c20d6b82f7650a466dae94582f0e5f0200d1
newExpand <magin0auto@gmail.com>
2023-05-16 04:54:42 +0900

    fix: 유저메뉴의 모달창이 테스트 중에는 눌렸는데 다시 해보니 안눌려서 코드 수정

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -22,6 +22,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
     const registerModal = useRegisterModal();
     const loginModel = useLoginModal();
     const rentModal = useRentModal();
+    const userMenuBtn = useRef(null);
     const userMenuModal = useRef(null);
 
     const [isOpen, setIsOpen] = useState(false);
@@ -40,9 +41,14 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
 
     useEffect(() => {
         const handleClick = (e: MouseEvent) => {
-            const userMenuRef = userMenuModal.current as any;
+            const userButtonRef = userMenuBtn.current as any;
+            const userModalRef = userMenuModal.current as any;
 
-            if (userMenuRef && !userMenuRef.contains(e.target)) {
+            if (
+                !userButtonRef.contains(e.target) &&
+                !userModalRef?.contains(e.target) &&
+                isOpen === true
+            ) {
                 setIsOpen(false);
             }
         };
@@ -51,7 +57,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
         return () => {
             document.removeEventListener("click", handleClick, { capture: true });
         };
-    }, []);
+    }, [isOpen]);
 
     return (
         <div className="relative">
@@ -64,7 +70,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
                 </div>
                 <div
                     onClick={toggleOpen}
-                    ref={userMenuModal}
+                    ref={userMenuBtn}
                     className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
                 >
                     <AiOutlineMenu />
@@ -74,7 +80,10 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
                 </div>
             </div>
             {isOpen && (
-                <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
+                <div
+                    ref={userMenuModal}
+                    className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm"
+                >
                     <div className="flex flex-col cursor-pointer">
                         {currentUser ? (
                             <>
